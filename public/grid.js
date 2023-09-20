20fa8338e1adc74b930dff342195943a9e5e31c2
Bram Wasti <bwasti@brams-air.lan>
2023-09-20 13:00:47 -0400

    fix grid

--- public/grid.js
+++ public/grid.js
@@ -1,64 +1,3 @@
-console.log("grid");
-
-// Your JSON data
-var jsonData = [{
-        id: 1,
-        list_id: 1,
-        name: "Pikachu",
-        data: '{"picture":"https://cdn03.ciceksepeti.com/cicek/kc4235987-1/XL/otografik-pikacu-pikachu-renkli-oto-sticker-11cm-x-11cm-kc4235987-1-bcfdccd4cc9544318b4c896de47c6707.jpg","info":"pika pika!"}',
-        elo: 1016.1234552326052,
-    },
-    {
-        id: 2,
-        list_id: 1,
-        name: "Charizard",
-        data: '{"picture":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png","info":"blows flames"}',
-        elo: 999.8765447673948,
-    },
-    {
-        id: 1,
-        list_id: 1,
-        name: "Pikachu",
-        data: '{"picture":"https://cdn03.ciceksepeti.com/cicek/kc4235987-1/XL/otografik-pikacu-pikachu-renkli-oto-sticker-11cm-x-11cm-kc4235987-1-bcfdccd4cc9544318b4c896de47c6707.jpg","info":"pika pika!"}',
-        elo: 1016.1234552326052,
-    },
-    {
-        id: 2,
-        list_id: 1,
-        name: "Charizard",
-        data: '{"picture":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png","info":"blows flames"}',
-        elo: 999.8765447673948,
-    },
-    {
-        id: 1,
-        list_id: 1,
-        name: "Pikachu",
-        data: '{"picture":"https://cdn03.ciceksepeti.com/cicek/kc4235987-1/XL/otografik-pikacu-pikachu-renkli-oto-sticker-11cm-x-11cm-kc4235987-1-bcfdccd4cc9544318b4c896de47c6707.jpg","info":"pika pika!"}',
-        elo: 1016.1234552326052,
-    },
-    {
-        id: 2,
-        list_id: 1,
-        name: "Charizard",
-        data: '{"picture":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png","info":"blows flames"}',
-        elo: 999.8765447673948,
-    },
-    {
-        id: 1,
-        list_id: 1,
-        name: "Pikachu",
-        data: '{"picture":"https://cdn03.ciceksepeti.com/cicek/kc4235987-1/XL/otografik-pikacu-pikachu-renkli-oto-sticker-11cm-x-11cm-kc4235987-1-bcfdccd4cc9544318b4c896de47c6707.jpg","info":"pika pika!"}',
-        elo: 1016.1234552326052,
-    },
-    {
-        id: 2,
-        list_id: 1,
-        name: "Charizard",
-        data: '{"picture":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png","info":"blows flames"}',
-        elo: 999.8765447673948,
-    },
-];
-
 // Function to create a card element
 function createCard(item, index) {
     var card = document.createElement("div");
@@ -78,7 +17,7 @@ function createCard(item, index) {
     card.appendChild(nameElement);
     card.appendChild(eloElement);
 
-    var data = JSON.parse(item.data);
+    var data = item.data;
 
     if (data.picture) {
         var pictureElement = document.createElement("img");
@@ -91,16 +30,18 @@ function createCard(item, index) {
 
 // Get the card container element
 var cardContainer = document.getElementById("card-container");
-
-// Create and append cards for each item in the JSON data
-jsonData.forEach(function(item, index) {
-    var card = createCard(item, index);
-    cardContainer.appendChild(card);
-});
-
 const queryString = window.location.search;
 const searchParams = new URLSearchParams(queryString);
 const listName = searchParams.get("listName");
+
+// Create and append cards for each item in the JSON data
+getSortedList(listName).then((data) => {
+  console.log(data);
+  data.forEach(function(item, index) {
+      var card = createCard(item, index);
+      cardContainer.appendChild(card);
+  });
+});
 const voteButton = document.getElementById("voteButton");
 const viewButton = document.getElementById("viewButton");
 viewButton.href = `list.html?listName=${listName}`;
