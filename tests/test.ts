61b518baec51697e2a6589bc1139618c73c8c649
Shannon Lin <swlsheltie@gmail.com>
2023-09-19 21:04:53 -0400

    stuff

--- tests/test.ts
+++ tests/test.ts
@@ -11,8 +11,30 @@ await api.changePassword("pokemon", "new_password", "pwd");
 console.assert(await api.checkPassword("pokemon", "pwd"));
 console.assert(!(await api.checkPassword("pokemon", "wrong_pass")));
 await api.createList("random", "your_list_password");
-await api.addItem("pokemon", { name: "Pikachu", data: "bleh" }, "pwd");
-await api.addItem("pokemon", { name: "Charizard", data: "bleh" }, "pwd");
+await api.addItem(
+  "pokemon",
+  {
+    name: "Pikachu",
+    data: {
+      picture:
+        "https://cdn03.ciceksepeti.com/cicek/kc4235987-1/XL/otografik-pikacu-pikachu-renkli-oto-sticker-11cm-x-11cm-kc4235987-1-bcfdccd4cc9544318b4c896de47c6707.jpg",
+      info: "pika pika!",
+    },
+  },
+  "pwd"
+);
+await api.addItem(
+  "pokemon",
+  {
+    name: "Charizard",
+    data: {
+      picture:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+      info: "blows flames",
+    },
+  },
+  "pwd"
+);
 await api.addItem("pokemon", { name: "Ekans", picture: "bleh" }, "pwd");
 await api.getPairForVoting("pokemon");
 await api.vote("pokemon", "Pikachu", "Ekans");
