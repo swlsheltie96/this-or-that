5f76fc45e1f74e39c4484105de4f4d07f7a55fc7
Shannon Lin <swlsheltie@gmail.com>
2023-09-25 11:32:54 -0400

    viewtypes

--- public/grid.js
+++ public/grid.js
@@ -1,39 +1,48 @@
 // Function to create a card element
 function createCard(item, index) {
-    var card = document.createElement("div");
-    card.classList.add("card");
-    card.classList.add("box");
+  var card = document.createElement("div");
+  card.classList.add("card");
+  card.classList.add("box");
+  card.classList.add("largeSquare");
 
-    var nameElement = document.createElement("span");
-    nameElement.textContent = item.name;
+  var nameElement = document.createElement("span");
+  nameElement.textContent = item.name;
 
-    var eloElement = document.createElement("div");
-    eloElement.style.padding = '0px 5px 5px 5px';
-    const description = item.data ? item.data.description ? item.data.description : '' : '';
-    eloElement.textContent = `${description} (Elo: ${item.elo.toFixed(2)})`;
+  var eloElement = document.createElement("div");
+  eloElement.classList.add("eloTag", "largeSquare");
 
-    var indexElement = document.createElement("span");
-    indexElement.textContent = `#${index}`;
-    indexElement.classList.add("index");
+  eloElement.style.padding = "0px 5px 5px 5px";
+  const description = item.data
+    ? item.data.description
+      ? item.data.description
+      : ""
+    : "";
+  eloElement.textContent = `${description} (Elo: ${item.elo.toFixed(2)})`;
 
-    const header = document.createElement('div');
-    header.classList.add('header');
-    header.classList.add('flex-justify');
-    header.appendChild(indexElement);
-    header.appendChild(nameElement);
-    card.appendChild(header)
+  var indexElement = document.createElement("span");
+  indexElement.textContent = `#${index}`;
+  indexElement.classList.add("index");
 
-    var data = item.data;
+  const header = document.createElement("div");
+  header.classList.add("header");
+  header.classList.add("flex-justify");
+  header.classList.add("largeSquare");
+  header.appendChild(indexElement);
+  header.appendChild(nameElement);
+  card.appendChild(header);
 
-    if (data.picture) {
-        var pictureElement = document.createElement("img");
-        pictureElement.src = data.picture;
-        pictureElement.classList.add('inner-box')
-        card.appendChild(pictureElement);
-    }
-    card.appendChild(eloElement);
+  var data = item.data;
 
-    return card;
+  if (data.picture) {
+    var pictureElement = document.createElement("img");
+    pictureElement.src = data.picture;
+    pictureElement.classList.add("inner-box");
+    pictureElement.classList.add("largeSquare");
+    card.appendChild(pictureElement);
+  }
+  card.appendChild(eloElement);
+
+  return card;
 }
 
 // Get the card container element
@@ -41,16 +50,45 @@ var cardContainer = document.getElementById("card-container");
 const queryString = window.location.search;
 const searchParams = new URLSearchParams(queryString);
 const listName = searchParams.get("listName");
-document.getElementById('listName').textContent = listName;
+document.getElementById("listName").textContent = listName;
 
 // Create and append cards for each item in the JSON data
 getSortedList(listName).then((data) => {
-  data.forEach(function(item, index) {
-      var card = createCard(item, index);
-      cardContainer.appendChild(card);
+  data.forEach(function (item, index) {
+    var card = createCard(item, index);
+    cardContainer.appendChild(card);
   });
+  loadViewTypes();
 });
 const voteButton = document.getElementById("voteButton");
 const viewButton = document.getElementById("viewButton");
 viewButton.href = `list.html?listName=${listName}`;
 voteButton.href = `vote.html?listName=${listName}`;
+
+function loadViewTypes() {
+  const smallSquare = document.getElementById("smallSquare");
+  const largeSquare = document.getElementById("largeSquare");
+  smallSquare.addEventListener("click", () => {
+    let largeStyle = document.querySelectorAll(".largeSquare");
+    if (!smallSquare.classList.contains("clicked")) {
+      smallSquare.classList.add("clicked");
+      largeSquare.classList.remove("clicked");
+    }
+    largeStyle.forEach((el) => {
+      el.classList.remove("largeSquare");
+      el.classList.add("smallSquare");
+    });
+  });
+
+  largeSquare.addEventListener("click", () => {
+    let smallStyle = document.querySelectorAll(".smallSquare");
+    if (!largeSquare.classList.contains("clicked")) {
+      largeSquare.classList.add("clicked");
+      smallSquare.classList.remove("clicked");
+    }
+    smallStyle.forEach((el) => {
+      el.classList.remove("smallSquare");
+      el.classList.add("largeSquare");
+    });
+  });
+}
